73a57d42f2465d6a88779f8a5aa8c49360a6fa2c
michinoku61 <michinoku61@gmail.com>
2019-08-11 12:11:56 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,11 @@
+# 省略
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
