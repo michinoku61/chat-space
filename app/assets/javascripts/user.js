25d2ff9b7742a28ef27a740104bd98f81fe1ddcb
michinoku61 <michinoku61@gmail.com>
2019-08-25 15:29:23 +0900

    インクリメンタルサーチの実装

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,45 @@
+$(function(){
+
+  let search_list = $('#user-search-result');
+
+  function appendUser(user){
+    let html = 
+    `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+    </div>`
+    search_list.append(html);
+    }
+
+  function appendErrMsgToHTML(msg) {
+    let html = `<p class="chat-group-user__name">${msg}</p>`
+    search_list.append(html);
+    }
+
+  
+
+  $('#user-search-field').on("keyup",function(){
+    let input = $('#user-search-field').val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users){
+      $('#user-search-result').empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendErrMsgToHTML("一致するユーザーはいません");
+      }
+    })
+    .fail(function() {
+      alert('error');
+    })
+  });
+});
